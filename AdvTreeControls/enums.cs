7ba10edb9ef4db3b4c57fd76d2728ed0b168a845
ncdat <ncdat@FVN-LAP-39>
2022-02-18 10:32:28 +0700

    Add project files.

---
+++ AdvTreeControls/enums.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvTreeControls
+{
+	public enum DrawSelectionMode
+	{
+		None, Active, Inactive, FullRowSelect
+	}
+
+	public enum TreeSelectionMode
+	{
+		Single, Multi, MultiSameParent
+	}
+
+	public enum NodePosition
+	{
+		Inside, Before, After
+	}
+}
---
+++ SampleApp/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace SampleApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeViewAdv1 = new AdvTreeControls.TreeViewAdv();
+            this.SuspendLayout();
+            // 
+            // treeViewAdv1
+            // 
+            this.treeViewAdv1.BackColor = System.Drawing.SystemColors.Window;
+            this.treeViewAdv1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.treeViewAdv1.DragDropMarkColor = System.Drawing.Color.Black;
+            this.treeViewAdv1.LineColor = System.Drawing.SystemColors.ControlDark;
+            this.treeViewAdv1.Location = new System.Drawing.Point(12, 12);
+            this.treeViewAdv1.Model = null;
+            this.treeViewAdv1.Name = "treeViewAdv1";
+            this.treeViewAdv1.SelectedNode = null;
+            this.treeViewAdv1.Size = new System.Drawing.Size(278, 324);
+            this.treeViewAdv1.TabIndex = 0;
+            this.treeViewAdv1.Text = "treeViewAdv1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeViewAdv1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AdvTreeControls.TreeViewAdv treeViewAdv1;
+    }
+}
+
